0c0156b8afd6cd461a3d28e8ea8f2217a7a71126
Stefan Majewsky <stefan.majewsky@sap.com>
2016-10-12 15:04:31 +0200

    read config file

--- main.go
+++ main.go
@@ -1,3 +1,43 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+//Config represents the content of the config file.
+type Config struct {
+	DriveGlobs []string `yaml:"drives"`
+}
+
+func main() {
+	err := actualMain()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
+
+func actualMain() error {
+	//expect one argument (config file name)
+	if len(os.Args) != 2 {
+		return fmt.Errorf("Usage: %s <config-file>\n", os.Args[0])
+	}
+
+	//read config file
+	configBytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		return err
+	}
+	var config Config
+	err = yaml.Unmarshal(configBytes, &config)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%#v\n", config)
+	return nil
+}
